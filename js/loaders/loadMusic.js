488971e90a40b5a7ec65062de3ab70c72eddb8fd
LuigZC <luigcarrasqueira@gmail.com>
2020-06-20 15:08:18 -0300

    Update loadMusic.js

--- js/loaders/loadMusic.js
+++ js/loaders/loadMusic.js
@@ -2,7 +2,7 @@ import MusicPlayer from "../MusicPlayer.js"
 import {loadJSON} from "./loadJSON.js";
 
 export function loadMusicSheet(name) {
-    return loadJSON("/musics/" + name + ".json").then(function(musicSheet) {
+    return loadJSON("./musics/" + name + ".json").then(function(musicSheet) {
         const musicPlayer = new MusicPlayer();
 
         for (const [name, track] of Object.entries(musicSheet)) musicPlayer.addTrack(name, track.url);
